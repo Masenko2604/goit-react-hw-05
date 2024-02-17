e3591997e190f770ba2692823835c8a534d43a4b
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-02-17 16:55:01 +0100

    update.app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,43 @@
-function App() {
-  return <>App</>;
-}
-export default App;
+import { Routes, Route, NavLink } from "react-router-dom";
+import clsx from 'clsx';
+import HomePages from "path/to/pages/HomePages";
+import MovieCast from "path/to/pages/MovieCast";
+import MovieReviews from "path/to/pages/MovieReviews";
+import MoviesDetailsPage from "path/to/pages/MoviesDetailsPage";
+import MoviesPage from "path/to/pages/MoviesPage";
+import NotFound from "path/to/pages/NotFound";
+import css from './App.module.css';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
+
+export const App = () => {
+  return (
+    <div>
+      <nav className={css.nav}>
+        <NavLink to="/" className={buildLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/about" className={buildLinkClass}>
+          About
+        </NavLink>
+        <NavLink to="/products" className={buildLinkClass}>
+          Products
+        </NavLink>
+      </nav>
+  
+      <Routes>
+        <Route path="/" element={<HomePages />} />
+        <Route path="/movieCast" element={<MovieCast />} />
+        <Route path="/movieReviews" element={<MovieReviews />} />
+        <Route path="/moviesDetailsPage" element={<MoviesDetailsPage />} />
+        <Route path="/MoviesPage" element={<MoviesPage />} />
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+    </div>
+  );
+};
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 // Імпорт стилів нормалізації
 import "modern-normalize";
 import App from "./components/App";
@@ -7,6 +8,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
